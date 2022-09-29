7d11e3c0268e1a62fe0dafc34860abb951bb9615
137042 <mare137042@gmail.com>
2022-09-29 10:07:42 +0900

    :tada: new proj

---
+++ src/main/java/kr/ac/kumoh/Saessak_Server/SaessakServerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.Saessak_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaessakServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaessakServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/kumoh/Saessak_Server/SaessakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.Saessak_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaessakServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
